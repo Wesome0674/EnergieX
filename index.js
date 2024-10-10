c0234357204e7cfa9727c67466a471a5a393b5f5
Killian.bld <105119247+Wesome0674@users.noreply.github.com>
2024-10-10 15:03:07 +0200

    fkgouifr_uy

--- index.js
+++ index.js
@@ -1,53 +1,75 @@
+// Gestion du défilement doux pour les liens d'ancrage
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+// Informations sur les cartes produits
 var card = document.querySelector(".card-product");
 
 var card_info = [
   {
-    reviews: "2050 reviews",
+    reviews: "2050 avis",
     img: "/assets/img/png/mochup1.png",
-    taste: "Fruit du dragon et mente",
+    taste: "Fruit du dragon et menthe",
+    fruit: "/assets/img/png/menteDragon.png",
   },
   {
-    reviews: "3000 reviews",
+    reviews: "3000 avis",
     img: "/assets/img/png/mochup2.png",
-    taste: "Citron et Gingembre épicé",
+    taste: "Citron et gingembre épicé",
+    fruit: "/assets/img/png/menteDragon.png",
   },
   {
-    reviews: "1200 reviews",
+    reviews: "1200 avis",
     img: "/assets/img/png/mochup3.png",
     taste: "Pomme verte et kiwi",
+    fruit: "/assets/img/png/menteDragon.png",
   },
   {
-    reviews: "1200 reviews",
+    reviews: "1200 avis",
     img: "/assets/img/png/mochup4.png",
     taste: "Framboise et litchi",
+    fruit: "/assets/img/png/menteDragon.png",
   },
   {
-    reviews: "1200 reviews",
+    reviews: "1200 avis",
     img: "/assets/img/png/mochup5.png",
-    taste: "Orange Sanguine et goyave",
+    taste: "Orange sanguine et goyave",
+    fruit: "/assets/img/png/menteDragon.png",
   },
   {
-    reviews: "1200 reviews",
+    reviews: "1200 avis",
     img: "/assets/img/png/mochup6.png",
-    taste: "Cerise Noir et grenade",
+    taste: "Cerise noire et grenade",
+    fruit: "/assets/img/png/menteDragon.png",
   },
   {
-    reviews: "1200 reviews",
+    reviews: "1200 avis",
     img: "/assets/img/png/mochup7.png",
-    taste: "Myrtille et Acai",
+    taste: "Myrtille et açaï",
+    fruit: "/assets/img/png/menteDragon.png",
   },
   {
-    reviews: "1200 reviews",
+    reviews: "1200 avis",
     img: "/assets/img/png/mochup8.png",
     taste: "Ananas et noix de coco",
+    fruit: "/assets/img/png/menteDragon.png",
   },
   {
-    reviews: "1200 reviews",
+    reviews: "1200 avis",
     img: "/assets/img/png/mochup9.png",
-    taste: "Peche et fruit de la passion",
+    taste: "Pêche et fruit de la passion",
+    fruit: "/assets/img/png/menteDragon.png",
   },
 ];
 
+// Insertion dynamique des cartes produits dans le DOM
 document.getElementById("test1").innerHTML = card_info
   .map(
     (card) =>
@@ -63,14 +85,16 @@ document.getElementById("test1").innerHTML = card_info
               </div>
               <p class="body-font">${card.reviews}</p>
             </div>
-            <p class="body-font2">2.99$</p>
+            <p class="body-font2">2,99€</p>
           </div>
           <div class="product-img">
            <img  src=${card.img} alt="" />
           </div>
           <div class="product_info">
             <p style="max-width: 200px;" class="uppercase">${card.taste}</p>
-            <button class="button-primary">ADD TO CART</button>
+            <a href="/product.html">
+              <button class="button-primary">ACHETER</button>
+            </a>
           </div>
         </div>`
   )
