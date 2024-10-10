667b0b8819e8893f04a41980454291e27da14f9f
Killian.bld <105119247+Wesome0674@users.noreply.github.com>
2024-10-10 16:27:47 +0200

    final commit thursday

---
+++ changeProduct.js
@@ -0,0 +1,15 @@
+// Sélectionne tous les éléments avec la classe 'input-variant'
+document.querySelectorAll('.input-variant').forEach(selectElement => {
+    // Ajoute un écouteur d'événement 'change' à chaque élément select
+    selectElement.addEventListener('change', (ev) => {
+      
+      // Récupère la valeur sélectionnée (l'URL)
+      var selectedPage = ev.currentTarget.value;
+      console.log(selectedPage);
+  
+      // Si une option valide est sélectionnée, redirige vers cette URL
+      if (selectedPage) {
+        window.location.href = selectedPage;
+      }
+    });
+  });
--- index.js
+++ index.js
@@ -18,54 +18,63 @@ var card_info = [
     img: "/assets/img/png/mochup1.png",
     taste: "Fruit du dragon et menthe",
     fruit: "/assets/img/png/menteDragon.png",
+    produit: "/product.html#items1",
   },
   {
     reviews: "3000 avis",
     img: "/assets/img/png/mochup2.png",
     taste: "Citron et gingembre épicé",
     fruit: "/assets/img/png/menteDragon.png",
+    produit: "/product.html#items2",
   },
   {
     reviews: "1200 avis",
     img: "/assets/img/png/mochup3.png",
     taste: "Pomme verte et kiwi",
     fruit: "/assets/img/png/menteDragon.png",
+    produit: "/product.html#items3",
   },
   {
     reviews: "1200 avis",
     img: "/assets/img/png/mochup4.png",
     taste: "Framboise et litchi",
     fruit: "/assets/img/png/menteDragon.png",
+    produit: "/product.html#items4",
   },
   {
     reviews: "1200 avis",
     img: "/assets/img/png/mochup5.png",
     taste: "Orange sanguine et goyave",
     fruit: "/assets/img/png/menteDragon.png",
+    produit: "/product.html#items5",
   },
   {
     reviews: "1200 avis",
     img: "/assets/img/png/mochup6.png",
     taste: "Cerise noire et grenade",
     fruit: "/assets/img/png/menteDragon.png",
+    produit: "/product.html#items6",
   },
   {
     reviews: "1200 avis",
     img: "/assets/img/png/mochup7.png",
     taste: "Myrtille et açaï",
     fruit: "/assets/img/png/menteDragon.png",
+    produit: "/product.html#items7",
   },
   {
     reviews: "1200 avis",
     img: "/assets/img/png/mochup8.png",
     taste: "Ananas et noix de coco",
     fruit: "/assets/img/png/menteDragon.png",
+    produit: "/product.html#items8",
   },
   {
     reviews: "1200 avis",
     img: "/assets/img/png/mochup9.png",
     taste: "Pêche et fruit de la passion",
     fruit: "/assets/img/png/menteDragon.png",
+    produit: "/product.html#items9",
   },
 ];
 
@@ -92,10 +101,12 @@ document.getElementById("test1").innerHTML = card_info
           </div>
           <div class="product_info">
             <p style="max-width: 200px;" class="uppercase">${card.taste}</p>
-            <a href="/product.html">
+            <a href=${card.produit}>
               <button class="button-primary">ACHETER</button>
             </a>
           </div>
         </div>`
   )
   .join("");
+
+ 
